67c42c8ed7b06aa5dddbaf76217fb39bd90c68dc
burhanuddinbaig <burhan.uddin.baig1@gmail.com>
2022-09-05 14:59:31 +0500

    Latest code

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-require('dotenv').config(); 
-const Discord = require('discord.js'); 
+const { REST, Routes } = require('discord.js');
 
-const client = new Discord.Client(); 
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken('');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(''), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds,
+                                      GatewayIntentBits.GuildMessages,
+                                      // GatewayIntentBits.MessageContent,
+                                      // GatewayIntentBits.GuildMembers
+                                    ] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//make sure this line is the last line
-client.login(process.env.CLIENT_TOKEN); //login bot using token
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('');
+
+// client.on('message', msg => {
+//   if (msg.content === 'ping') {
+//     msg.reply('Pong!');
+//       }
+//     });
+
+// //make sure this line is the last line
+// client.login(process.env.CLIENT_TOKEN); //login bot using token
